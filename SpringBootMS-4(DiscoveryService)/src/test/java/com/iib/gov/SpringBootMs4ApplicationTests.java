d9ee94be79cddfe7ef73c29cdb5987fbec7bf082
soumya <soumya@soumya.iib.local>
2023-10-06 12:09:07 +0530

    commiting springBoot-DiscoveryService

---
+++ SpringBootMS-4(DiscoveryService)/src/test/java/com/iib/gov/SpringBootMs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iib.gov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMs4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
